42f9039a7b75c4497543ecc6f7f95d6b6dabd606
orloveste <orlovevm@yahoo.com>
2020-04-15 17:37:48 +0300

    Integer_types - Example - sum of individual digits from number byTony :)

--- src/main/java/Integer_Types/Example/Add_digits_fomNumber.java
+++ src/main/java/Integer_Types/Example/Add_digits_fomNumber.java
@@ -1,9 +1,26 @@
 package Integer_Types.Example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Add_digits_fomNumber {
     public static void main(String[] args) {
+        // v1 byTony
+        Scanner reader = new Scanner(System.in);
+        //Integer x = 525;
+        Integer x = reader.nextInt();
+
+        String s = x.toString();
+        ArrayList<String>stringArrayList = new ArrayList<String>();
+        for (String s1 : s.split("")) {
+            stringArrayList.add(s1);
+        }
+        int stringNumber = Integer.parseInt(stringArrayList.get(0));
+        int stringNumber1 = Integer.parseInt(stringArrayList.get(1));
+        int stringNumber2 = Integer.parseInt(stringArrayList.get(2));
+        System.out.println(stringNumber+stringNumber1+stringNumber2);
+    }
+    /*public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your number from 100 to 999: ");
         int num = scanner.nextInt();
@@ -17,5 +34,5 @@ public class Add_digits_fomNumber {
         } else {
             System.out.println("Bye! Your number is out of range");
         }
-    }
+    }*/
 }
