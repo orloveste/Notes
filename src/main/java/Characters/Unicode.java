dcdf5eb7624b5354f7eea71755e85ed1a963a255
orloveste <orlovevm@yahoo.com>
2020-04-19 11:31:42 +0300

    Characters - unicode & shortcut - how tu find int of char

--- src/main/java/Characters/Unicode.java
+++ src/main/java/Characters/Unicode.java
@@ -2,15 +2,17 @@ package Characters;
 
 public class Unicode {
     public static void main(String[] args) {
-        char ch = 'e';
-        System.out.println(ch);
-        ch -= 'a';//null
-        System.out.println("-=a : " +ch);
-        ch++;// executed
-        System.out.println("++ : "+ch);
-        ch += 'b';//null
-        System.out.println("+=B : "+ch);
+        int mystery = 'z';
+        System.out.println(mystery);
 
+//        char ch = 'e';
+//        System.out.println(ch);
+//        ch -= 'a';//null
+//        System.out.println("-=a : " +ch);
+//        ch++;// executed
+//        System.out.println("++ : "+ch);
+//        ch += 'b';//null
+//        System.out.println("+=B : "+ch);
 
 //        char ch = '\u0031' + '\u0035';
 //        System.out.println(ch);
