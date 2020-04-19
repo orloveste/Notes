9385e15eb5c643856e3872cee85aad2ff877857f
orloveste <orlovevm@yahoo.com>
2020-04-19 11:20:58 +0300

    Characters - unicode & shortcut - sum

---
+++ src/main/java/Characters/Unicode.java
@@ -0,0 +1,10 @@
+package Characters;
+
+public class Unicode {
+    public static void main(String[] args) {
+        char ch = '\u0031' + '\u0035';
+        System.out.println(ch);
+        char test = '\u0066';
+        System.out.println(test);
+    }
+}
