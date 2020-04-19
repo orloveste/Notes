5547ae558eeec5527d72ab0c874b71530949264a
orloveste <orlovevm@yahoo.com>
2020-04-19 11:28:11 +0300

    Characters - unicode & shortcut - exception on format next

--- src/main/java/Characters/Unicode.java
+++ src/main/java/Characters/Unicode.java
@@ -2,9 +2,19 @@ package Characters;
 
 public class Unicode {
     public static void main(String[] args) {
-        char ch = '\u0031' + '\u0035';
+        char ch = 'e';
         System.out.println(ch);
-        char test = '\u0066';
-        System.out.println(test);
+        ch -= 'a';//null
+        System.out.println("-=a : " +ch);
+        ch++;// executed
+        System.out.println("++ : "+ch);
+        ch += 'b';//null
+        System.out.println("+=B : "+ch);
+
+
+//        char ch = '\u0031' + '\u0035';
+//        System.out.println(ch);
+//        char test = '\u0066';
+//        System.out.println(test);
     }
 }
