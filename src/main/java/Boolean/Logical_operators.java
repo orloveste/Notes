b6352663cf8c60a14532a42f8a5c0671a737a2a4
orloveste <orlovevm@yahoo.com>
2020-04-23 14:32:27 +0300

    Logical operators - NOT

---
+++ src/main/java/Boolean/Logical_operators.java
@@ -0,0 +1,10 @@
+package Boolean;
+
+public class Logical_operators {
+    public static void main(String[] args) {
+        boolean f = false; // f is false
+        System.out.println(f);
+        boolean t = !f;    // t is true
+        System.out.println(t);
+    }
+}
