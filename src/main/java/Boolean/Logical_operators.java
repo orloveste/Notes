8e7ad48c9bc294238c1984ce8cdffdd0456716a4
orloveste <orlovevm@yahoo.com>
2020-04-23 14:35:05 +0300

    Logical operators - AND

--- src/main/java/Boolean/Logical_operators.java
+++ src/main/java/Boolean/Logical_operators.java
@@ -2,9 +2,20 @@ package Boolean;
 
 public class Logical_operators {
     public static void main(String[] args) {
-        boolean f = false; // f is false
+//        Logical operators - AND
+        boolean b1 = false && false; // false
+        System.out.println(b1);
+        boolean b2 = false && true;  // false
+        System.out.println(b2);
+        boolean b3 = true && false;  // false
+        System.out.println(b3);
+        boolean b4 = true && true;   // true
+        System.out.println(b4);
+
+//        Logical operators - NOT
+        /*boolean f = false; // f is false
         System.out.println(f);
         boolean t = !f;    // t is true
-        System.out.println(t);
+        System.out.println(t);*/
     }
 }
