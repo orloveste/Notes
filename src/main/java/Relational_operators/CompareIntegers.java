4f4e53a09fa24f1326a3b9b93485d455982b406d
orloveste <orlovevm@yahoo.com>
2020-04-24 10:21:46 +0300

    Relational operators - Comparing integer numbers

---
+++ src/main/java/Relational_operators/CompareIntegers.java
@@ -0,0 +1,27 @@
+package Relational_operators;
+
+public class CompareIntegers {
+    public static void main(String[] args) {
+        /*int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+
+        boolean oneIsOne = one == one; // true
+
+        boolean res1 = two <= three; // true
+        boolean res2 = two != four;  // true
+        boolean res3 = two > four;   // false
+        boolean res4 = one == three; // false
+        System.out.println(oneIsOne);
+        System.out.println(res1);
+        System.out.println(res2);
+        System.out.println(res3);
+        System.out.println(res4);*/
+
+        int number = 1000;
+        boolean result = number + 10 > number + 9; // 1010 > 1009 is true
+        System.out.println(result);
+
+    }
+}
