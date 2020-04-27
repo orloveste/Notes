5d03bc1ccf3c835a78616ba51deb19d20b8fd6e8
orloveste <orlovevm@yahoo.com>
2020-04-27 11:13:01 +0300

    If not take in consideration being false - Conditional statement

---
+++ src/main/java/Conditional_statement/output.java
@@ -0,0 +1,17 @@
+package Conditional_statement;
+
+public class output {
+    public static void main(String[] args) {
+        int a = 2;
+        if (a <= 0) {// always false
+
+            if(a == 0) {
+                System.out.println("1 ");
+            }
+            else {
+                System.out.println("2 ");
+            }
+        }
+        System.out.println("3 ");
+    }
+}
