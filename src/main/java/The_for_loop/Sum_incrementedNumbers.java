ab19811349646c3559c45566d0920f10c9a93352
orloveste <orlovevm@yahoo.com>
2020-04-28 11:30:05 +0300

    sum of the integer numbers from 1 to 10 (inclusive) using the for-loop

---
+++ src/main/java/The_for_loop/Sum_incrementedNumbers.java
@@ -0,0 +1,16 @@
+package The_for_loop;
+
+public class Sum_incrementedNumbers {
+    public static void main(String[] args) {
+//        sum of the integer numbers from 1 to 10 (inclusive) using the for-loop.
+        int startIncl = 1, endExcl = 11;
+
+        int sum = 0;
+        for (int i = startIncl; i < endExcl; i++) {
+            sum += i;
+            System.out.println(i + " ");
+        }
+
+        System.out.println(sum); // it prints "55"
+    }
+}
