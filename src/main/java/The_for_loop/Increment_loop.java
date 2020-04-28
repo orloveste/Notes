8e9bb39c2f505143ceb517fb1351156d3cd74a39
orloveste <orlovevm@yahoo.com>
2020-04-28 11:24:30 +0300

    Increment loop - for-loop

---
+++ src/main/java/The_for_loop/Increment_loop.java
@@ -0,0 +1,10 @@
+package The_for_loop;
+
+public class Increment_loop {
+    public static void main(String[] args) {
+        int n = 9;
+        for (int i = 0; i <= n; i++) {
+            System.out.print(i + " "); // here, space is used to separate numbers
+        }
+    }
+}
