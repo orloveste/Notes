b967943ce4c59dcc24bb64914801192233870c82
orloveste <orlovevm@yahoo.com>
2020-05-05 09:55:41 +0300

    Type casting ex

---
+++ src/main/java/Testing.java
@@ -0,0 +1,7 @@
+public class Testing {
+    public static void main(String[] args) {
+        float f = (float) (20d + 20.02f); //float f = 20d + 20.02f;
+        long n = 10 + 2L;
+        byte b = (byte) (n + 5); //byte b = n + 5;
+    }
+}
