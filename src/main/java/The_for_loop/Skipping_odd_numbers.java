07ffa787fffbb46c0dcdd6db368e86872e986c49
orloveste <orlovevm@yahoo.com>
2020-05-08 12:35:08 +0300

    continue; - used in loop

---
+++ src/main/java/The_for_loop/Skipping_odd_numbers.java
@@ -0,0 +1,13 @@
+package The_for_loop;
+
+public class Skipping_odd_numbers {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 != 0) {
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
