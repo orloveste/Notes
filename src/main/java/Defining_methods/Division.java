47d36771265c9c0c3e2c811044b564e30186f4c9
orloveste <orlovevm@yahoo.com>
2020-05-13 14:03:57 +0300

    methods - Division.java

---
+++ src/main/java/Defining_methods/Division.java
@@ -0,0 +1,18 @@
+package Defining_methods;
+
+import java.util.Scanner;
+
+public class Division {
+
+    public static double divide(long a, long b) {
+        return (double) a / b;
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final long a = scanner.nextLong();
+        final long b = scanner.nextLong();
+        System.out.println(divide(a, b));
+    }
+}
